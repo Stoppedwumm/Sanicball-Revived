7081dae7c1d035ccf37ead5df6af655447c0d2b6
Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>
2026-06-19 17:35:30 +0200

    Update GameVersion constants for new release

--- Subprojects/Core/GameVersion.cs
+++ Subprojects/Core/GameVersion.cs
@@ -3,15 +3,15 @@
     public class GameVersion
     {
         //The current version as a float, for checking which version is newest
-        public const float AS_FLOAT = 0.91f;
+        public const float AS_FLOAT = 0.100f;
 
         //To differentiate between testing builds and release builds
         public const bool IS_TESTING = false;
 
         //As a string, for displaying on the UI
-        public const string AS_STRING = "alpha-revived v0.9.1";
+        public const string AS_STRING = "alpha-revived v0.10.0";
 
         //Something stupid, usually unique for every version
-        public const string TAGLINE = "if life gives you lemons, just ignore them - stoppedwumm";
+        public const string TAGLINE = "spinnnnnnnnnnn";
     }
 }
